4dae040bc52cd8f88fd257f198de01f901fb9f00
bhavya091213 <bhavya091213@gmail.com>
2025-03-29 15:47:27 -0400

    webcam frame upload

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const cors = require("cors");
+
+//kmulter
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+// app
 const app = express();
-const { spawn } = require("child_process").spawn;
+
+// Enable CORS
+app.use(cors());
+
+// dotenv
 require("dotenv").config();
 const port = process.env.SERVER || 3000;
 
+// spawn async (child_process)
+const { spawn } = require("child_process").spawn;
+
 app.get("/", (req, res) => {
+  console.log("faggot");
+  res.send("hello wrld");
+});
+app.post("/uploadFrame", upload.single("frame"), (req, res) => {
   res.send(port);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listenxing on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
