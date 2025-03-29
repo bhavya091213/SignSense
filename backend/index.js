5e9abe34aa626fa3ef53bc2892ecd26c23f64d97
bhavya091213 <bhavya091213@gmail.com>
2025-03-29 13:37:00 -0400

    bhavya push

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const { spawn } = require("child_process").spawn;
+require("dotenv").config();
+const port = process.env.SERVER || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(port);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listenxing on port ${port}`);
 });
